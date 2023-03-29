e4de77297dcd00b26d2b4876504867ea105b9457
alif riduwan <alifriduwan.a@gmail.com>
2023-03-29 13:29:51 +0700

    Array

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
+import "fmt"
 
 func main() {
-	f, err := os.Open("file.txt")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer f.Close()
-	//process file
-	//read file
-	//calculate...
+	// var num1 [2]int
+	// fmt.Println(num1) // [0 0] เพราะไม่มีการกำหนดค่า
+
+	var num1 [2]int
+	num1[0] = 1
+	num1[1] = 2
+	fmt.Println(len(num1), num1, num1[0], num1[1])
+
+	num2 := [3]int{14, 17, 15}
+	fmt.Println(len(num2), num2)
 }
