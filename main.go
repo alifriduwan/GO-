eecc0d1c6c47b25fe5b02f56e619dfb5afc2d6cf
alif riduwan <alifriduwan.a@gmail.com>
2023-03-29 17:22:51 +0700

    Pointer with struct

--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 
 import "fmt"
 
+type Point struct {
+	X int
+	Y int
+}
+
 func main() {
 	i := 10
 	j := &i
 	i = 12
 	*j = 15
 	fmt.Println(*j, i)
+
+	p := &Point{X: 100, Y: 200}
+	(*p).X = 300
+	fmt.Println(p)
 }
