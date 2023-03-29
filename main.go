b3c234379286bd584baf97fb3571b99449cb3b60
alif riduwan <alifriduwan.a@gmail.com>
2023-03-29 13:07:24 +0700

    switch

--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 )
 
 func main() {
-	// n := 17
-	if n := 17; n%2 == 0 { //สารมารถกำหนดค่าเริ่มต้นตรงตำแหน่งเช็คเงื่อนไข
-		fmt.Println("Even number")
-	} else {
-		fmt.Println("Odd number")
+	os := runtime.GOOS
+
+	switch os {
+	case "darwin":
+		fmt.Println("OS X")
+	case "linux":
+		fmt.Println("Linux")
+	default:
+		fmt.Println(os)
 	}
 }
