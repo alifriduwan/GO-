d1f36ffdba3c2c84f965b7317a1f859794d6cc92
alif riduwan <alifriduwan.a@gmail.com>
2023-03-29 17:19:37 +0700

    Pointer

--- main.go
+++ main.go
@@ -1,51 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-type Printer interface {
-	Print() string //method Print() return string
-}
-type PDF struct {
-	Content string
-}
-
-func (p PDF) Print() string {
-	return p.Content
-}
-
-type Excel struct {
-	Cells []string
-}
-
-func (e Excel) Print() string {
-	return strings.Join(e.Cells, ",")
-}
+import "fmt"
 
 func main() {
-	docs := []Printer{
-		PDF{Content: "Hello PDF"},
-		Excel{Cells: []string{"A", "B"}},
-	}
-
-	for _, doc := range docs {
-		fmt.Println(doc.Print())
-	}
-
-	// empty interface{}
-	docs2 := []interface{}{
-		PDF{Content: "Hello PDF"},
-		Excel{Cells: []string{"A", "B"}},
-	}
-
-	fmt.Println(docs2)
-
-	docs3 := []any{
-		PDF{Content: "Hello PDF"},
-		Excel{Cells: []string{"A", "B"}},
-	}
-
-	fmt.Println(docs3)
+	i := 10
+	j := &i
+	i = 12
+	*j = 15
+	fmt.Println(*j, i)
 }
