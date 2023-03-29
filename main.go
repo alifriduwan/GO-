9f7386663167f5e4a19c7fee2a672ccfb0a50d87
alif riduwan <alifriduwan.a@gmail.com>
2023-03-29 15:46:30 +0700

    การใช้ errors.New

--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 	"math"
 )
 
+var ErrorInvalidRadius = errors.New("radius must be positive")
+
 func Area(r float64) (float64, error) {
 	if r <= 0 {
-		return 0.0, errors.New("radius must be positive")
+		return 0.0, ErrorInvalidRadius
 	}
 
 	return math.Pi * r * r, nil
@@ -16,7 +18,7 @@ func Area(r float64) (float64, error) {
 
 func main() {
 	result, err := Area(-17)
-	if err != nil {
+	if errors.Is(err, ErrorInvalidRadius) {
 		fmt.Println(err)
 	} else {
 		fmt.Println(result)
